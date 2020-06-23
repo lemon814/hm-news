36f5b71a494fef9f8bf3fd077f24952658637376
lemon814 <13821501560@163.com>
2020-06-23 14:49:58 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
